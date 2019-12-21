c6b7af286992ac278c7609714ee8c4f7fff9faf0
kevinGwon <developerGwon@gmail.com>
2019-12-21 13:49:31 +0900

    Fix webpack.config

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-let app = express();
-app.get('/public/index.html', (req, res) => res.send('HELLO FROM EXPRESS'));
-app.use(express.static('public'));
+const app = express();
+app.get('/dist/index.html', (req, res) => res.send('HELLO FROM EXPRESS'));
+app.use(express.static('dist'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const merge = require('webpack-merge');
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const isDev = process.env.NODE_ENV === 'development' ? true : false;
-const webpackObj = isDev ? require('./webpack.dev') : require('./webpack.prod');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
+// webpeack config
+const isDev = process.env.NODE_ENV === 'development' ? true : false;
+const configCustom = isDev ? require('./webpack.dev') : require('./webpack.prod');
 const config = {
   entry: {app: path.resolve(__dirname, 'src', 'app.js')},
   output: {
@@ -80,7 +81,7 @@ const config = {
 };
 
 console.log('-----------------------');
-console.log(isDev);
+console.log(process.env.NODE_ENV);
 console.log('-----------------------');
 
-module.exports = merge(config, webpackObj);
+module.exports = merge(config, configCustom);
