15bd5d6a5b5c9cca7e410419bbdea1f038dd0e66
kevinGwon <developerGwon@gmail.com>
2019-12-30 20:04:18 +0900

    Fix List - 순서정렬 추가

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
 function List({ list }) {
+  // 순서 정렬
+  list.length === 10 &&
+    list.sort((prev, next) => {
+      if (parseInt(prev.rank, 10) > parseInt(next.rank, 10)) {
+        return 1;
+      }
+      if (parseInt(prev.rank, 10) < parseInt(next.rank, 10)) {
+        return -1;
+      }
+      return 0;
+    });
+  console.log(list);
   return (
     <ul className='movie-list'>
       {list.map(item => (
         <li key={item.data.Data[0].Result[0].DOCID}>
           <a href='#'>
+            <div className='thumb'>
+              <img
+                src={item.data.Data[0].Result[0].posters.split('|')[0]}
+                alt=''
+              />
+            </div>
             <strong className='title'>{item.data.Query}</strong>
             <p className='cont'>{item.data.Data[0].Result[0].plot}</p>
-            <div className='thumb'>
-              <img src={item.data.Data[0].Result[0].posters} alt='' />
-            </div>
           </a>
         </li>
       ))}
