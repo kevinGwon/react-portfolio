5775f6b620dcac7bafefe89d3d97dcd9d4ee1084
kevinGwon <developerGwon@gmail.com>
2020-01-07 04:02:20 +0900

    Fix 마크업 구조 변경

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import HeaderContainer from './containers/HeaderContainer';
 import ListContainer from './containers/ListContainer';
 
+// CATEGORY
+import { POPULARITY, AVERAGE } from './reducer/list';
+
 function App() {
+  const { lists } = useSelector(store => store.list, []);
   return (
     <>
       <HeaderContainer />
-      <ListContainer />
+
+      <div className="article">
+        <div className="l-wrap">
+          <h2>인기 영화</h2>
+          <ListContainer category={POPULARITY} lists={lists.popularity} />
+
+          <h2>높은 평점</h2>
+          <ListContainer category={AVERAGE} lists={lists.average} />
+        </div>
+      </div>
     </>
   );
 }
