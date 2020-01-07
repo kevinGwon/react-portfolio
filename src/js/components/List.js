ba8944e2efbe925b2343fd2654038e0414b85f2d
kevinGwon <developerGwon@gmail.com>
2020-01-07 18:05:27 +0900

    Update swiper style

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -4,16 +4,25 @@ import Swiper from 'swiper';
 function List({ lists }) {
   useEffect(() => {
     setTimeout(() => {
-      let swiper = new Swiper(`.swiper-container-${lists.category}`, {
-        effect: 'coverflow',
+      const swiper = new Swiper(`.swiper-container-${lists.category}`, {
         lazy: true,
         centeredSlides: true,
         slidesPerView: 3,
         spaceBetween: 5,
         grabCursor: true,
+        parallax: true,
+        speed: 800,
+        effect: 'coverflow',
+        coverflowEffect: {
+          rotate: 50, // Slide rotate in degrees
+          stretch: 0, // Stretch space between slides (in px)
+          depth: 100, // Depth offset in px (slides translate in Z axis)
+          modifier: 1, // Effect multipler
+          slideShadows: true, // Enables slides shadows
+        },
         pagination: {
           el: '.swiper-pagination',
-          type: 'bullets',
+          dynamicBullets: true,
           clickable: true,
         },
         navigation: {
@@ -21,37 +30,53 @@ function List({ lists }) {
           prevEl: '.swiper-button-prev',
         },
       });
-    }, 0);
+    }, 300);
   }, [lists.category]);
   return (
     <>
-      <div className={`swiper-container swiper-container-${lists.category}`}>
-        <div className="swiper-wrapper">
-          {lists.list.map(item => (
-            <div key={item.id} className="swiper-slide">
-              <a href="#">
-                <div className="thumb">
-                  <img
-                    data-src={
-                      item.posterImage.indexOf('null') === -1
-                        ? item.posterImage
-                        : 'http://placehold.it/500x747?text=Preparing image'
-                    }
-                    className="swiper-lazy"
-                    alt=""
-                  />
+      <div className="movie-container">
+        <div
+          className="movie-container-bg"
+          style={
+            lists.list[0] && {
+              backgroundImage: `url('${lists.list[0].bgImage}')`,
+            }
+          }
+        ></div>
+        <div className="l-wrap">
+          <div className="h2">{lists.category}</div>
+          <div
+            className={`swiper-container swiper-container-${lists.category}`}
+          >
+            <div className="swiper-wrapper">
+              {lists.list.map(item => (
+                <div key={item.id} className="swiper-slide">
+                  <a href="#">
+                    <div className="thumb">
+                      <img
+                        data-src={
+                          item.posterImage.indexOf('null') === -1
+                            ? item.posterImage
+                            : 'http://placehold.it/500x747?text=Preparing image'
+                        }
+                        className="swiper-lazy"
+                        alt=""
+                      />
+                      <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
+                    </div>
+                  </a>
                 </div>
-              </a>
+              ))}
             </div>
-          ))}
+
+            {/* Pagination */}
+            <div className="swiper-pagination"></div>
+
+            {/* Arrow */}
+            <div className="swiper-button-next"></div>
+            <div className="swiper-button-prev"></div>
+          </div>
         </div>
-
-        {/* Pagination */}
-        <div className="swiper-pagination"></div>
-
-        {/* Arrow */}
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
       </div>
     </>
   );
