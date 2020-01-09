910e6f56ac0165101e92bf1203fbcfdab8753300
kevinGwon <developerGwon@gmail.com>
2020-01-09 16:34:57 +0900

    Add Footer

---
+++ src/js/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer id="footer" className="footer">
+      @copyright
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/js/containers/FooterContainer.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import Footer from '../components/Footer';
+
+function footer() {
+  return <Footer />;
+}
+
+export default footer;
