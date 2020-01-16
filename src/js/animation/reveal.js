be8d78f2f31e7c21af93c556543920c5db8df37a
kevinGwon <developerGwon@gmail.com>
2020-01-16 14:11:11 +0900

    Fix reveal.js
    - selector

--- src/js/animation/reveal.js
+++ src/js/animation/reveal.js
@@ -1,9 +1,12 @@
 import { LOADING_OUT } from '../reducer/load';
 
 export const reveal = () => dispatch => {
+  const $app = document.querySelector('#app');
+
   dispatch({ type: LOADING_OUT });
-  IG.$app.classList.remove('is-loading');
-  TweenMax.from(IG.$app.querySelector('.movie-article'), IG.DUR * 3, {
+  $app.classList.remove('is-loading');
+
+  TweenMax.from($app.querySelector('.article'), IG.DUR * 3, {
     delay: IG.DUR,
     ease: IG.EASE,
     autoAlpha: 0,
