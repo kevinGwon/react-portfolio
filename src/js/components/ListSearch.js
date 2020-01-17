b39488b782f03be8083a704d433d557f201df90b
kevinGwon <developerGwon@gmail.com>
2020-01-17 19:45:28 +0900

    Fix LoadingCricle Component 분리

--- src/js/components/ListSearch.js
+++ src/js/components/ListSearch.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import filterArray from '../modules/filterArray';
 import { Link } from 'react-router-dom';
+import LoadingCircle from './LoadingCircle';
 
 function SearchList({ lists }) {
   // console.log(`5. state = ${lists.isLoading} [ListSearch.js]`);
@@ -31,21 +32,7 @@ function SearchList({ lists }) {
           )}
         </ul>
       ) : (
-        <div className="loading loading--circle" title="Loading">
-          <svg version="1.1" x="0px" y="0px" viewBox="0 0 50 50">
-            <path d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
-              <animateTransform
-                attributeType="XML"
-                attributeName="transform"
-                type="rotate"
-                from="0 25 25"
-                to="360 25 25"
-                dur="0.6s"
-                repeatCount="indefinite"
-              />
-            </path>
-          </svg>
-        </div>
+        <LoadingCircle />
       )}
     </div>
   );
