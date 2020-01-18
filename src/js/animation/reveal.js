2277b5e6aeac9bdcc615e6e3ab0574bc03b0ec71
kevinGwon <developerGwon@gmail.com>
2020-01-19 04:35:25 +0900

    Fix reveal.js - 엘리먼트를 못찾는 이슈

--- src/js/animation/reveal.js
+++ src/js/animation/reveal.js
@@ -2,9 +2,12 @@ import { LOADING_OUT } from '../reducer/load';
 
 export const reveal = () => dispatch => {
   const $app = document.querySelector('#app');
+  const $article = $app.querySelector('.movie-article');
 
   dispatch({ type: LOADING_OUT });
   $app.classList.remove('is-loading');
+
+  if ($article === null) return;
 
   TweenMax.from($app.querySelector('.movie-article'), IG.DUR * 3, {
     delay: IG.DUR,
